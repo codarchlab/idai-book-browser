23c2a1aa7bb41f1c3b39a8f4b4a8928039b8c4d2
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-08-24 15:33:57 +0200

    Refactor for readability.

--- js/controllers.js
+++ js/controllers.js
@@ -4,50 +4,45 @@
 
 'use strict';
 
-/* Controllers */
-
 angular.module('idaiBookBrowser.controllers',[])
 
 /**
  * api doc of main controller
+ * $scope
+ *   currentPageIndex - technical index of a book page which is focused (shown big). starts with 0.
+ *   pageMinis - array of items of which each represents a minified book page
  */
 .controller('MainController',	[ '$scope', '$location', '$http',
 	function ($scope, $location, $http) {
 
+		var defaultNrItems2Load = 5; // each time (for infinite scrolling)
 
-
-		$scope.currentPage = 0;
+		$scope.currentPageIndex = 0;
 		$scope.pageMinis = [];
+
+		var book = {}; // the json book data
 
 		$scope.loadMore = function() {
 
+			var nrPagesNotLoaded = book.pages.length - $scope.pageMinis.length;
 
-			var newItemCountMax = 5;
+			var nrPagesToLoad = defaultNrItems2Load;
+			if (nrPagesNotLoaded<defaultNrItems2Load) nrPagesToLoad=nrPagesNotLoaded;
 
-			var last = $scope.pageMinis.length;
-
-			var diff = $scope.book.pages.length-$scope.pageMinis.length;
-
-			var newItemCount = newItemCountMax;
-			if (diff<newItemCountMax) newItemCount=diff;
-
-
-			for(var i = 0; i < newItemCount; i++) {
-				$scope.pageMinis.push($scope.book.pages[last + i]);
+			for(var i = 0; i < nrPagesToLoad; i++) {
+				$scope.pageMinis.push(book.pages[$scope.pageMinis.length + i]);
 			}
 		};
 
 
 		$http.get('/sample_book.json').success(function(data){
 
-			$scope.book=data;
+			book=data;
 
 			$scope.loadMore();
 
-			$scope.leftHandSidePage=$scope.book.pages[$scope.currentPage];
-			$scope.rightHandSidePage=$scope.book.pages[$scope.currentPage+1];
-
-
+			$scope.leftHandSidePage=book.pages[$scope.currentPageIndex];
+			$scope.rightHandSidePage=book.pages[$scope.currentPageIndex+1];
 		});
 	}
 ]);
